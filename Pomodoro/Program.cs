dbf6a42941c5476409a7f2f570d6fdf623913d3c
RicardoSilva0227 <ricardo.silvaa2002@gmail.com>
2024-04-11 19:14:26 +0100

    Add project files.

---
+++ Pomodoro/Program.cs
@@ -0,0 +1,84 @@
+﻿using NAudio.Wave;
+
+class PomodoroTimer
+{
+    static void Main(string[] args)
+    {
+        int workTime = 25;
+        int breakTime = 5;
+        int pomodoroCounter = 0;
+
+        Console.WriteLine("Pomodoro Timer");
+        Console.WriteLine("Press enter to start the timer!");
+        Console.ReadLine();
+
+        while (true)
+        {
+
+            Console.WriteLine($"Pomodoro {++pomodoroCounter}: Work for {workTime} minutes.");
+            Thread.Sleep(workTime * 60 * 1000);
+
+            playBreakStartSound();
+
+            Console.WriteLine($"Take a {breakTime}-minute break.");
+            Thread.Sleep(breakTime * 60 * 1000);
+
+            playRestartingSound();
+
+            
+        }
+
+        Console.WriteLine("Pomodoro session ended. Goodbye!");
+    }
+
+    static void playBreakStartSound()
+    {
+
+        string SoundFilePath = "StopBell.wav";
+        FileStream Wav_file_stream = File.OpenRead(SoundFilePath);
+        WaveStream ws_raw = new WaveFileReader(Wav_file_stream);
+        ws_raw = WaveFormatConversionStream.CreatePcmStream(ws_raw);
+        WaveOutEvent Output = new WaveOutEvent();
+
+        Wav_file_stream.Seek(0, SeekOrigin.Begin);
+
+        Output.Init(ws_raw);
+        Output.Play();
+
+        // Wait for the audio to finish playing
+        while (Output.PlaybackState == PlaybackState.Playing)
+        {
+            Thread.Sleep(100); 
+        }
+
+
+        Wav_file_stream.Close();
+        ws_raw.Close();
+        Output.Dispose();
+    }
+
+    static void playRestartingSound()
+    {
+        string SoundFilePath = "restart.wav";
+        FileStream Wav_file_stream = File.OpenRead(SoundFilePath);
+        WaveStream ws_raw = new WaveFileReader(Wav_file_stream);
+        ws_raw = WaveFormatConversionStream.CreatePcmStream(ws_raw);
+        WaveOutEvent Output = new WaveOutEvent();
+
+        Wav_file_stream.Seek(0, SeekOrigin.Begin);
+
+        Output.Init(ws_raw);
+        Output.Play();
+
+        // Wait for the audio to finish playing
+        while (Output.PlaybackState == PlaybackState.Playing)
+        {
+            Thread.Sleep(100);
+        }
+
+        Wav_file_stream.Close();
+        ws_raw.Close();
+        Output.Dispose();
+    }
+}
+
